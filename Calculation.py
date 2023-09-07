5b534712e89ec6a12176f3834ea808ef41a7f744
Tyler Wagner <tylerrwagner38@gmail.com>
2023-09-07 12:31:18 -0500

    Updated Files
    Updated the file that allows file read write.

--- Calculation.py
+++ Calculation.py
@@ -1,42 +1,83 @@
+"""
+Code written by Tyler Wagner
+Used by: Tyler Wager, Jordan Ricks, Alexis Davis
+"""
+
 import time
-import math
+
 
 def main():
-    prime1 = int(input("Enter a prime number: "))
-    prime2 = int(input("Enter a prime number: "))
+    prime1 = int(input("Enter a number: "))
+    prime2 = int(input("Enter a number: "))
 
-    primeAnswer = multiply(prime1, prime2)
-    print (primeAnswer)
+    # get the answer and the time it took to multiply from the multiply method
+    primeAnswer, multiplyTime = multiply(prime1, prime2)
 
-    final = factor(primeAnswer)
-    print (final)
+    print(primeAnswer)
+
+    # open the output file
+    outFile = open("outputFile.txt", "w")
+
+    # cast the multiplyTime flat to a string
+    strMultTime = str(multiplyTime)
+
+    # Write to file
+    outFile.write(f"Time to multiply: {strMultTime}")
+
+    if _isPrime(primeAnswer) is True:
+        factor(primeAnswer)
+
+    else:
+        print(f"{primeAnswer} is not prime factoring now")
+        factor(primeAnswer)
+
+    # Get the factor time and the list of factors form the factor method
+    results = factor(primeAnswer)
+    final, factorTime = results[:-1], results[-1]
+    print(final)
+
+    # cast the factorTime to a string
+    strFactorTime = str(factorTime)
+
+    # Write the output to the file
+    outFile.write(f"\nTime to factor: {strFactorTime}")
+
+    outFile.close()
 
 
 def multiply(prime1, prime2):
     startTimer = time.perf_counter()
     primeAnswer = prime1 * prime2
     stopTimer = time.perf_counter()
-    print(f"Calculated the two prime numbers in {stopTimer - startTimer:0.10f} seconds")
-    return primeAnswer
+    finTimer = stopTimer - startTimer
+    print(f"Calculated the two prime numbers in {finTimer:0.10f} seconds")
+    return primeAnswer, finTimer
 
 
-def factor(primeAnswer, divisor = 2):
+def factor(primeAnswer, divisor=2):
     factors = []
     startFacTimer = time.perf_counter()
     while primeAnswer > 1 and divisor * divisor <= primeAnswer:
-        if primeAnswer% divisor == 0:
+        if primeAnswer % divisor == 0:
             factors.append(divisor)
             return factors + factor(primeAnswer // divisor, divisor)
         divisor += 1
-        print(divisor)
+
+        if primeAnswer > 1:
+            factors.append(primeAnswer)
+
+        # Only uncomment the line before this if you need to check for errors
+        # print(divisor)
     stopFacTimer = time.perf_counter()
-    print(f"Finished in {stopFacTimer - startFacTimer: .04f} seconds")
+    finTimer = stopFacTimer - startFacTimer
+    print(f"Finished in {finTimer: .04f} seconds")
+    return factors + [finTimer]
 
 
 def _isPrime(primeAnswer):
     if primeAnswer <= 1:
         return False
-    if primeAnswer <=3:
+    if primeAnswer <= 3:
         return True
     if primeAnswer % 2 == 0 or primeAnswer % 3 == 0:
         return False
@@ -44,10 +85,8 @@ def _isPrime(primeAnswer):
     while i * i <= primeAnswer:
         if primeAnswer % i == 0 or primeAnswer % (i + 2) == 0:
             return False
-        i+= 6
+        i += 6
     return True
-
-
 
 
 if __name__ == "__main__":
